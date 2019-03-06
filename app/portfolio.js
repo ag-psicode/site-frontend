d87af6bf73d583ee16b3eb9efaeae1bcfcf66867
Victor Nascimento <victor.nascimento.ns@gmail.com>
2019-03-06 16:26:40 -0300

    add carousel

--- app/portfolio.js
+++ app/portfolio.js
@@ -8,3 +8,18 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
+
+import { tns } from "tiny-slider/src/tiny-slider"
+
+var slider = tns({
+    "container": ".portfolio-slider",
+    "items": 3,
+    "center": false,
+    "loop": true,
+    "swipeAngle": false,
+    "speed": 400,
+    "axis": "vertical",
+    "controls": false,
+    "nav": true,
+    "mouseDrag": true
+});
