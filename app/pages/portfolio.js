37bb2894c36c3d986f4de530e50f7b2180df07d8
victornns <victor.nascimento.ns@gmail.com>
2019-03-21 09:05:09 -0300

    too much tweaking on the carrousel

--- app/pages/portfolio.js
+++ app/pages/portfolio.js
@@ -1,25 +1,79 @@
 /**
- * Application entry point
- */
+* Application entry point
+*/
 
 // Load application styles
 import 'styles/index.scss';
+import { tns } from "tiny-slider/src/tiny-slider"
 
 // ================================
 // START YOUR APP HERE
 // ================================
 
-import { tns } from "tiny-slider/src/tiny-slider"
+const is_mobile = function() {
+    let view_width = window.screen.width;
+    if(view_width < 768) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-var slider = tns({
-    "container": ".portfolio-slider",
-    "items": 3,
-    "center": false,
-    "loop": true,
-    "swipeAngle": false,
-    "speed": 400,
-    "axis": "vertical",
-    "controls": false,
-    "nav": true,
-    "mouseDrag": true
-});
+var portfolio_slider = {
+    init: function() {
+        let obj;
+
+        if(is_mobile()) {
+            obj = Object.assign(this.options.common, this.options.mobile);
+        } else {
+            obj = Object.assign(this.options.common, this.options.desktop);
+            document.addEventListener('wheel', this.findScrollDirection);
+        }
+
+        this.slider = tns(obj);
+    },
+    slider: {},
+    options: {
+        common: {
+            container: ".portfolio-slider",
+            swipeAngle: false,
+            speed: 400,
+            nav: false,
+            mouseDrag: true
+        },
+        mobile: {
+            items: 2,
+            center: true,
+            loop: false,
+            controls: false,
+            axis: "horizontal"
+        },
+        desktop: {
+            items: 3,
+            center: false,
+            loop: true,
+            controls: true,
+            axis: "vertical"
+        }
+    },
+    findScrollDirection: function() {
+        let delta;
+
+        if (event.wheelDelta){
+            delta = event.wheelDelta;
+        }else{
+            delta = -1 * event.deltaY;
+        }
+
+        if (delta < 0){
+            portfolio_slider.slider.goTo('prev');
+        }else if (delta > 0){
+            portfolio_slider.slider.goTo('next');
+        }
+    }
+}
+
+
+
+// Load
+portfolio_slider.init();
